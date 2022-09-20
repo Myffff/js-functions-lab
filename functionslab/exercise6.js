8ce5136c52265487aeeef3c58e4829980b28d121
Yifei Ma <634042441@qq.com>
2022-09-20 20:11:10 +0800

    Exercise 6.

--- functionslab/exercise6.js
+++ functionslab/exercise6.js
@@ -1,12 +1,14 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const completed = json // Complete this code
-
-    const accumulator = completed.reduce( (total, todo, index, array) => {
-        return {index: todo.userId}
-    }, 0)
-//    console.log(accumulator);
+    const completed1 = json.reduce( (accumu, todo, index, array) => {
+        const currAccumu= accumu[todo.userId] ? accumu[todo.userId]: 0
+        return {
+            ...accumu,
+            [todo.userId]:currAccumu + todo.completed
+        }
+    }, {})
+   console.log(completed1);
   })
   .catch(function(err) { 
     console.log(err);
