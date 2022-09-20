72513bd1e68f6913f419cbe7ced433d549e5f517
Yifei Ma <634042441@qq.com>
2022-09-20 17:55:10 +0800

    Exercise 1.

--- functionslab/exercise1.js
+++ functionslab/exercise1.js
@@ -1,10 +1,17 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const completed = null // Complete this code
-     completed.forEach( (todo, index) => {
-      console.log(`${todo.title} - ${todo.completed}`)
-    })
+     const completed = json // Complete this code
+     
+    //  completed.forEach( (todo, index) => {
+    //   console.log(`${todo.title} - ${todo.completed}`)
+    // })
+
+    const completed1 = completed.filter((todo, index) => {
+        
+        return todo.completed ==true;
+      });
+      console.log(completed1);
   })
   .catch(function(err) { 
     console.log(err);
